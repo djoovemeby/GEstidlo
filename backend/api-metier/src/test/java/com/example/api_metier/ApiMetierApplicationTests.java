b3463004422b70c0b65cf2f4022f8b3a4cc3e2e9
dimitrydesilias <158100419+dimitrydesilias@users.noreply.github.com>
2026-01-15 22:41:32 -0500

    feat: Initial setup with Camunda 7, Angular 18 and working configuration

---
+++ backend/api-metier/src/test/java/com/example/api_metier/ApiMetierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_metier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMetierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
